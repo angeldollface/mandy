3816813f34aad4a39aca1a27306f0ef52a8656df
✭ ANGEL DOLLFACE ✭ <112771657+angeldollface@users.noreply.github.com>
2023-06-29 15:16:13 +0200

    Delete nix.sh

--- installers/nix.sh
+++
@@ -1,9 +0,0 @@
-# Install the Rust toolchain.
-if ! [ -x "$(command -v cargo)" ]; then 
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-else
-    echo "Required toolchain installed, proceeding."
-fi
-
-# Install Mandy.
-cargo install --git https://github.com/angeldollface/mandy --force
