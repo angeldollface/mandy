ce93bf1d4e42026e0274b42ca04c8d110a4f6159
✭ ANGEL DOLLFACE ✭ <112771657+angeldollface@users.noreply.github.com>
2023-07-01 23:57:08 +0200

    Update compile.rs

--- src/modules/compile.rs
+++ src/modules/compile.rs
@@ -17,11 +17,6 @@ use super::errors::MandyError;
 /// a Mandy site's context.
 use super::context::SiteContext;
 
-/// Importing the method to get
-/// the base name of a file in a
-/// file path.
-use super::utils::get_name_base;
-
 /// Importing the method to compile
 /// SASS files into a CSS file from
 /// "./sass.rs".
@@ -34,12 +29,6 @@ use super::build_context::build_context;
 /// A function to retrieve site contexts from
 /// a Mandy site project.
 use super::get_context::get_site_contexts;
-
-/// A method to create the files for
-/// crawling bots of search engines.
-/// Provides hypersonic SEO for Mandy-powered
-/// sites.
-use super::crawlers::create_crawler_files;
 
 /// Creates files and renders them from all
 /// "SiteContext" instances.
@@ -65,24 +54,7 @@ pub fn compile_site(dir: &String) -> Result<(), MandyError> {
                     );
                 }
             };
-            let mut urls: Vec<String> = Vec::new();
-            let mut tl_domain: String = String::from("");
-            let mut baseurl: String = String::from("");
-            let mut freq: String = String::from("");
             for ctx in site_contexts {
-                tl_domain = ctx.clone().site["tlDomain"].clone();
-                freq = ctx.clone().site["updateFreq"].clone();
-                baseurl = ctx.clone().baseurl;
-                let file_url: &String = &ctx.file;
-                let mut dir_url: String = ctx.clone().dir;
-                let fname: String = get_name_base(file_url)[0].clone();
-                dir_url = dir_url.replace(dir, &"");
-                let mut final_url: String = format!("{}/{}", &dir_url, fname);
-                if &dir_url == &String::from(""){
-                    final_url = String::from("/");
-                }
-                else {}
-                urls.push(final_url.clone());
                 let build_op: Result<(), MandyError> = build_context(&ctx, dir);
                 match build_op {
                     Ok(_x) => {},
@@ -96,22 +68,6 @@ pub fn compile_site(dir: &String) -> Result<(), MandyError> {
                 };
             }
             match compile_sass_files(dir) {
-                Ok(_x) => {},
-                Err(e) => {
-                    return Err::<(), MandyError>(
-                        MandyError::new(
-                            &e.to_string()
-                        )
-                    );
-                }
-            }
-            match create_crawler_files(
-                &urls, 
-                &freq,
-                &baseurl, 
-                &tl_domain, 
-                dir
-            ){
                 Ok(_x) => {},
                 Err(e) => {
                     return Err::<(), MandyError>(
